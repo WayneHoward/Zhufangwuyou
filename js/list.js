abefd6c0c125b3c58c74552dde1e1c7c74845d40
WayneHoward <wayne_howard@outlook.com>
2020-11-27 11:44:05 +0800

    11-27 11:44

--- js/list.js
+++ js/list.js
@@ -1,69 +1,52 @@
 $(function(){
-    var aa = function(e){
+    //阻止页面滚动
+    var releaseScroll = function(e){
         e.preventDefault();
     }
+    
     $('.nav>li>a').each(function(index,item){
         touch.on(item,'tap',function(){
+            //导航字体改变
             $('.nav>li>a').css('color','rgb(102,102,102)');
             $(this).css('color','#ff8a00');
             // 阻止页面滚动
-            document.body.addEventListener('touchmove',aa, { passive: false }); 
-            switch(index){
-                case 0:
-                    $('.area1,.area2,.area3').hide();
-                    $('.area').slideDown('fast');
-                    $('.hid').fadeIn('fast');
-                    
-                    $('.down1').show();
-                    $('.up').hide();
-                    $(item).parent().find('.arrow-down .down1').hide();
-                    $(item).parent().find('.arrow-down .up').show();
-                    break;
-                case 1:
-                    $('.area,.area2,.area3').hide();
-                    $('.area1').slideDown('fast');
-                    $('.hid').fadeIn('fast');
-
-                    $('.down1').show();
-                    $('.up').hide();
-                    $(item).parent().find('.arrow-down .down1').hide();
-                    $(item).parent().find('.arrow-down .up').show();
-                    break;
-                case 2:
-                    $('.area1,.area,.area3').hide();
-                    $('.area2').slideDown('fast');
-                    $('.hid').fadeIn('fast');
-
-                    $('.down1').show();
-                    $('.up').hide();
-                    $(item).parent().find('.arrow-down .down1').hide();
-                    $(item).parent().find('.arrow-down .up').show();
-                    break;
-                case 3:
-                    $('.area1,.area2,.area').hide();
-                    $('.area3').slideDown('fast');
-                    $('.hid').fadeIn('fast');
-
-                    $('.down1').show();
-                    $('.up').hide();
-                    $(item).parent().find('.arrow-down .down1').hide();
-                    $(item).parent().find('.arrow-down .up').show();
-                    break;
+            document.body.addEventListener('touchmove',releaseScroll, { passive: false });
+            //再次点击隐藏板块
+            if($('.down').eq(index).is(':visible')){
+                //板块隐藏，蒙版隐藏
+                $('.down').slideUp('fast');
+                $('.hid').fadeOut('fast');
+                //导航字体改变
+                $('.nav>li>a').css('color','#666666');
+                //箭头朝上
+                $('.up').hide();
+                $('.down1').show();
+                //释放页面滚动
+                document.body.removeEventListener('touchmove',releaseScroll);
+            }else{
+                //板块出现
+                $('.down').hide();
+                $('.down').eq(index).slideDown('fast');
+                $('.hid').fadeIn('fast');
+                 //箭头改变
+                 $('.up').hide();
+                $('.down1').show();
+                $(item).parent().find('.arrow-down .up').show();
+                $(item).parent().find('.arrow-down .down1').hide();
             }
         });
     });
     touch.on($('.hid'),'tap',function(){
-        $('.area').slideUp('fast');
-        $('.area1').slideUp('fast');
-        $('.area2').slideUp('fast');
-        $('.area3').slideUp('fast');
+        //板块隐藏，蒙版隐藏
+        $('.down').slideUp('fast');
         $('.hid').fadeOut('fast');
+        //导航字体改变
         $('.nav>li>a').css('color','#666666');
         //箭头朝上
         $('.down1').show();
         $('.up').hide();
         //释放页面滚动
-        document.body.removeEventListener('touchmove',aa);
+        document.body.removeEventListener('touchmove',releaseScroll);
     });
     
     //区域单选
@@ -91,37 +74,17 @@ $(function(){
             $(item).find('.ok').show();
             $(item).css('border-color','#ff8a00');
         });
-    });
-    touch.on($('.area2 li:eq(0) a'),'tap',function(){
-        $(this).css('color','#ff8a00');
-        $('.ok').hide();
-        $('.area2 li .s2').css('border-color','#878787');
+        touch.on($('.area2 li:eq(0) a'),'tap',function(){
+            $(this).css('color','#ff8a00');
+            $('.ok').hide();
+            $('.area2 li .s2').css('border-color','#878787');
+        });
     });
 
     //更多单选
-    $('.area3 .fir,.sec,.thir ul li a').each(function(index,item){
-        touch.on($('.area3 .fir ul li a'),'tap',function(){
-            $('.area3 .fir ul li a').css({
-                'color':'#878787',
-                'border-color':'rgb(201,201,201)'
-            });
-            $(this).css({
-                'color':'#ff8a00',
-                'border-color':'#ff8a00'
-            });
-        });
-        touch.on($('.area3 .sec ul li a'),'tap',function(){
-            $('.area3 .sec ul li a').css({
-                'color':'#878787',
-                'border-color':'rgb(201,201,201)'
-            });
-            $(this).css({
-                'color':'#ff8a00',
-                'border-color':'#ff8a00'
-            });
-        });
-        touch.on($('.area3 .thir ul li a'),'tap',function(){
-            $('.area3 .thir ul li a').css({
+    $('.area3 .house ul li a').each(function(index,item){
+        touch.on($('.area3 .house').eq(index).find('ul li a'),'tap',function(){
+            $('.area3 .house ul li a').css({
                 'color':'#878787',
                 'border-color':'rgb(201,201,201)'
             });
